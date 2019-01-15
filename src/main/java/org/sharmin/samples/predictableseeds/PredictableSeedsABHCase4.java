596660b13801e09a9ce6eda3961863c9191da10f
Sharmin Afrose <sharminafrose@vt.edu>
2019-01-15 11:33:34 -0500

    removal of unnecessary files

--- src/main/java/org/sharmin/samples/predictableseeds/PredictableSeedsABHCase4.java
+++ src/main/java/org/sharmin/samples/predictableseeds/PredictableSeedsABHCase4.java
@@ -13,9 +13,9 @@ public class PredictableSeedsABHCase4 {
 
         Map<String,Byte> hm = new HashMap<String, Byte>();
         hm.put("aaa", new Byte((byte) 100));
-        hm.put("bbb", new Byte((byte) 100));
-        hm.put("ccc", new Byte((byte) 100));
-        hm.put("ddd", new Byte((byte) 100));
+        hm.put("bbb", new Byte((byte) 200));
+        hm.put("ccc", new Byte((byte) 300));
+        hm.put("ddd", new Byte((byte) 400));
 
         byte b = hm.get("aaa");
 
